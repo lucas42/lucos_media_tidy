ccc2e55101ed11844ac94ae6a5622de348fdfdb9
Luke Blaney <git@lukeblaney.co.uk>
2020-03-19 00:44:25 +0000

    revert rewrites to rowan's stuff as I found my old stuff

--- tidy.py
+++ tidy.py
@@ -3,6 +3,8 @@
 import requests, sys, json, os
 
 def trackExists(url):
+	if "rowan" in url:
+		return False
 	try:
 		trackres = requests.head(url)
 		return trackres.ok
@@ -23,6 +25,10 @@ while True:
 		print("Have read all tracks")
 		break
 	for track in data:
+
+		# First track has blank details, fix manually later
+		if (track['trackid'] == 1):
+			continue
 		if trackExists(track['url']):
 			#print("Track "+str(track["trackid"])+" exists")
 			continue
@@ -48,9 +54,9 @@ while True:
 		url = url.replace("...Here's", "Here's")
 		url = url.replace("Phill Collins %2526 Eric Clapton", "Phil Collins & Eric Clapton")
 		url = url.replace("Sting %2526 The Police", "Sting & The Police")
-		url = url.replace("black/ceol srl/artists/Sinead OConnor", "rowan/Moosic/Sinead O'Connor")
-		url = url.replace("black/ceol srl/artists/Keane", "import/rowan/Moosic/Keane")
-		url = url.replace("black/ceol srl/artists/Zutons, The/Zutons - Tired Of Hanging Around", "rowan/Moosic/Zutons, The/Tired Of Hanging Around (2006)")
+		url = url.replace("rowan/Moosic/Sinead O'Connor", "black/ceol srl/artists/Sinead OConnor")
+		url = url.replace("import/rowan/Moosic/Keane", "black/ceol srl/artists/Keane")
+		url = url.replace("rowan/Moosic/Zutons, The/Tired Of Hanging Around (2006)", "black/ceol srl/artists/Zutons, The/Zutons - Tired Of Hanging Around", )
 		url = url.replace("%252526 ", "& ")
 		url = url.replace("%2526 ", "& ")
 		url = url.replace("%26 ", "&")
@@ -71,16 +77,12 @@ while True:
 		url = url.replace("Jackson, Micheal", "Jackson, Michael")
 		url = url.replace("black/ceol srl/artists/levellers/Too Real", "rowan/Moosic/Levellers, The/Too Real (EP) (1998)")
 		url = url.replace("black/ceol srl/artists/levellers/Just The One EP", "rowan/Moosic/Levellers, The/Just The One (EP) (1995)")
-		url = url.replace("black/ceol srl/artists/Snow Patrol", "rowan/Moosic/Snow Patrol")
-		url = url.replace("black/ceol srl/artists/View, The/The View - Hats Off To The Buskers", "rowan/Moosic/View, The/Hats Off To The Buskers (2007)")
-		url = url.replace("black/ceol srl/artists/Kano", "rowan/Moosic/Kano/Home Sweet Home (2005)")
-		url = url.replace("black/ceol srl/artists/Paris/Sonic Jihad", "rowan/Moosic/Paris/Sonic Jihad (2003)")
-		url = url.replace("black/ceol srl/artists/Van the Man", "rowan/Moosic/Van Morrison")
-		url = url.replace("Snow Patrol/Snow Patrol - ", "Snow Patrol/")
-		url = url.replace("/When It's All Over We Still Have To Clear Up/", "/When It's All Over We Still Have To Clear Up (2001)/")
-		url = url.replace("/Songs For Polarbears", "/Songs For Polarbears (1998)")
-		url = url.replace("/Eyes Open", "/Eyes Open (2006)")
-		url = url.replace("black/ceol srl/artists/Leftfield/Leftism", "rowan/Moosic/Leftfield/Leftism (1995)")
+		url = url.replace("rowan/Moosic/Snow Patrol", "black/ceol srl/artists/Snow Patrol")
+		url = url.replace("rowan/Moosic/View, The/Hats Off To The Buskers (2007)", "black/ceol srl/artists/View, The/The View - Hats Off To The Buskers")
+		url = url.replace("rowan/Moosic/Kano/Home Sweet Home (2005)", "black/ceol srl/artists/Kano")
+		url = url.replace("rowan/Moosic/Paris/Sonic Jihad (2003)", "black/ceol srl/artists/Paris/Sonic Jihad")
+		url = url.replace("rowan/Moosic/Van Morrison", "black/ceol srl/artists/Van the Man")
+		url = url.replace("rowan/Moosic/Leftfield/Leftism (1995)", "black/ceol srl/artists/Leftfield/Leftism")
 		url = url.replace("Media to sort/Savage Garden Discography", "artists/Savage Garden")
 		url = url.replace("Media to sort/Camper Van Beethoven", "artists/Camper Van Beethoven")
 		url = url.replace("Media to sort/Sigur Ros", "artists/Sigur Ros")
@@ -88,7 +90,7 @@ while True:
 		url = url.replace("rowan/Moosic/Game, The/The Documentary (2005)", "black/ceol srl/artists/Game/Game - The Documentary")
 		url = url.replace("rowan/Moosic/Mamas & The Papas/Dream A Little Dream Of Me.mp3", "black/ceol srl/artists/Mamas and the Papas, The/Mamas And The Papas - Dream A Little Dream Of Me.mp3")
 		url = url.replace("rowan/Moosic/Muse", "black/ceol srl/artists/Muse")
-		url = url.replace(".wma.ogg", ".wma")
+		url = url.replace(".wma", ".wma.ogg")
 		url = url.replace("99%", "99%25")
 		url = url.replace("rowan/Moosic/Kanye West", "black/ceol srl/artists/Kanye West")
 		url = url.replace("Jackson, Micheal", "Jackson, Michael")
@@ -99,32 +101,30 @@ while True:
 		url = url.replace("PussyCat Dolls &", "PussyCat Dolls %2526 ")
 		url = url.replace("import/rowan/Moosic/Keane", "black/ceol srl/artists/Keane")
 		url = url.replace("rowan/Moosic/Sinead O'Connor", "black/ceol srl/artists/O'Connor, Sinéad")
+		url = url.replace("rowan/Moosic/Watercress", "black/ceol srl/artists/Watercress")
+		url = url.replace("Watercress/Bummer (2001)", "Watercress/Bummer")
+		url = url.replace("rowan/Moosic/Animaniacs, The", "black/ceol srl/Kids")
+		url = url.replace("rowan/Moosic/Divers (French)", "black/ceol srl/Kids/Divers (French)")
+		url = url.replace("rowan/Moosic/Game, The/The Documentary (2005)", "black/ceol srl/artists/Game/Game - The Documentary")
+		url = url.replace("rowan/Moosic/Mamas & The Papas/California Dreaming.mp3", "black/ceol srl/artists/Mamas and the Papas, The/Mamas And The Papas - California Dreaming.mp3")
+		url = url.replace("rowan/Moosic/Muse", "black/ceol srl/artists/Muse")
+		url = url.replace("Absolution (2003)", "Muse - Absolution")
+		url = url.replace("Origin Of Symmetry (2001)", "Muse - Origin Of Symmetry")
+		url = url.replace("rowan/Moosic/Maximo Park/A Certain Trigger (2005)", "black/ceol srl/artists/Maximo Park/Maximo Park - A Certain Trigger")
+		url = url.replace("rowan/Moosic/Levellers, The/Too Real (EP) (1998)", "black/ceol srl/artists/levellers/Too Real")
+		url = url.replace("rowan/Moosic/Levellers, The/Just The One (EP) (1995)", "black/ceol srl/artists/levellers/Just The One EP")
+		url = url.replace("Black Holes And Revelations (2006)", "Muse - Black Holes And Revelations")
+		url = url.replace("Final Straw (2003)", "Final Straw")
+		url = url.replace("When It's All Over We Still Have To Clear Up (2001)", "When It's All Over We Still Have To Clear Up")
+		url = url.replace("Songs For Polarbears (1998)", "Songs For Polarbears")
+		url = url.replace("Eyes Open (2006)", "Eyes Open")
+		url = url.replace("Tired Of Hanging Around (2006)", "Tired Of Hanging Around")
+		url = url.replace("rowan/Moosic/Ukulele Orchestra of GB", "black/ceol srl/artists/Ukulele Orchestra of GB")
 		url = url.replace("", "")
 		url = url.replace("", "")
 		url = url.replace("", "")
 		url = url.replace("", "")
 		url = url.replace("", "")
-
-		tracksToDelete = [
-			1239,  # Old flac file I don't care about
-			# Duplicate files:
-			14079,
-			3727,
-			17169,
-			17170,
-			17171,
-			17172,
-			17175,
-			17178,
-			17179,
-			17180,
-			17182,
-			17184,
-			17189,
-			17192,
-		]
-		if track['trackid'] in tracksToDelete:
-			continue
 
 		if trackExists(url):
 			trackdata = {
